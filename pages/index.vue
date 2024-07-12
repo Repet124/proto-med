f46169a3d3f480b4a0bc401fd366ee921482e96e
Oleg Bondarenko <hlop4ik124@gmail.com>
2024-07-12 20:46:29 +0300

    init commit

---
+++ pages/index.vue
@@ -0,0 +1,12 @@
+<template>
+	<h1>Информационная система</h1>
+	<NuxtLink to="/profile">Личный кабинет</NuxtLink>
+</template>
+
+<script setup>
+	
+</script>
+
+<style scoped>
+	
+</style>
---
+++ pages/profile/index.vue
@@ -0,0 +1,14 @@
+<template>
+	<h1>Личный кабинет</h1>
+	<NuxtLink to="/pacients/my">Мои пациенты</NuxtLink>
+	<NuxtLink to="/pacients/all">Справочник пациентов</NuxtLink>
+	<NuxtLink to="/templates">Справочник пациентов</NuxtLink>
+</template>
+
+<script setup>
+	
+</script>
+
+<style scoped>
+	
+</style>
