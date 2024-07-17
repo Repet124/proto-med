689873851f145cc46c23cb3043642c802f598862
Oleg Bondarenko <hlop4ik124@gmail.com>
2024-07-17 21:42:55 +0300

    index styles

--- layouts/default.vue
+++ layouts/default.vue
@@ -2,8 +2,10 @@
 <div class="min-h-screen bg-slate-300">
 
 	<div class="bg-slate-800 border-b border-slate-500">
-		<header class="container h-16 flex items-center text-white text-xl">
-			<h1><slot name="title">Информационная система</slot></h1>
+		<header class="container h-16 flex items-center">
+			<p class="text-white text-xl">
+				<slot name="title">Информационная система</slot>
+			</p>
 		</header>
 	</div>
 	
--- pages/index.vue
+++ pages/index.vue
@@ -1,7 +1,13 @@
 <template>
 <NuxtLayout>
 
-	<NuxtLink to="/profile">Личный кабинет</NuxtLink>
+	<h1 class="text-3xl">Модули</h1>
+	<ul class="text-xl">
+		<li class="before:content-['-'] before:mr-2">
+			<NuxtLink class="underline text-slate-800 hover:text-slate-500" to="/profile">Личный кабинет</NuxtLink>
+		</li>
+		<li class="before:content-['-'] before:mr-2">Другие модули...</li>
+	</ul>
 
 </NuxtLayout>
 </template>
