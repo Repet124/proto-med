f7486936b807eb2f6d4d5e2f0ed73c2fd82dd3e6
Oleg Bondarenko <hlop4ik124@gmail.com>
2024-07-18 20:02:18 +0300

    personal data list

--- pages/patients/[patient_id]/index.vue
+++ pages/patients/[patient_id]/index.vue
@@ -3,10 +3,21 @@
 
 	<template #title>Личный кабинет</template>
 	<Btn type="back" />
-	<h1 class="text-3xl">Пациент: {{ patient.name }}</h1>
+	<h1 class="font-bold text-3xl">Пациент: {{ patient.name }}</h1>
 
+	<section class="bg-white p-2 border border-slate-600 rounded-md">
+		<h2 class="text-2xl">Персональные данные</h2>
+		<ul>
+			<li v-for="item in personalDataProperties">
+				<span class="font-bold text-md">{{ item.label }}</span>
+				:
+				<span class="text-md">{{ patient[item.key] }}</span>
+			</li>
+		</ul>
+	</section>
+
+	<h2 class="text-2xl">Список приёмов</h2>
 	<Btn type="link" :to="`/patients/${id}/receptions/create`">Добваить новый приём</Btn>
-	<h2>Список приёмов</h2>
 	<ul class="flex flex-col gap-4">
 		<li v-for="reception in patient.receptions">
 			<ReceptionItem
@@ -22,6 +33,13 @@
 <script setup>
 	const id = useRoute().params.patient_id;
 	const {data: patient} = await useFetch(`/api/patients/${id}`);
+
+	var personalDataProperties = [
+		{key: 'firstName', label: 'Имя'},
+		{key: 'lastName', label: 'Фамилия'},
+		{key: 'email', label: 'Почта'},
+		{key: 'phone', label: 'Телефон'}
+	]
 </script>
 
 <style scoped>
