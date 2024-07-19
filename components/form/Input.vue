c4302b104b858a3174bfa97e0e307e89c59d6497
Oleg Bondarenko <hlop4ik124@gmail.com>
2024-07-19 18:17:48 +0300

    Input was styled. Label component

--- components/form/Input.vue
+++ components/form/Input.vue
@@ -1,11 +1,9 @@
 <template>
-	
-	<label class="form-label">
-		<span class="form-title">{{ title }}</span>
+<FormLabel :title="title">
 
 		<input
-			class="form-input"
-			:class="{'form-input_err': !!error}"
+			class="w-full border border-slate-600 rounded-md px-2 py-1"
+			:class="{'text-red-600': !!error}"
 			:type="type ? type : 'text'"
 			:name="name"
 			:placeholder="placeholder ? placeholder : 'Type new value...'"
@@ -13,8 +11,8 @@
 			@input="$emit('update:modelValue', $event.target.value === '' ? null : $event.target.value)"
 		>
 		<span v-if="!!error" class="form-err">{{error}}</span>
-	</label>
-	
+
+</FormLabel>
 </template>
 
 <script>
---
+++ components/form/Label.vue
@@ -0,0 +1,10 @@
+<template>
+	<label class="flex flex-col gap-2 w-full">
+		<span>{{ title }}:</span>
+		<slot/>
+	</label>
+</template>
+
+<script setup>
+	var { title } = defineProps(['title']);
+</script>
