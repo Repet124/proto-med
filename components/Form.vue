248b5cae6d7a86e823a71623da10a1c0811568b9
Oleg Bondarenko <hlop4ik124@gmail.com>
2024-07-19 16:51:04 +0300

    fields to form component

---
+++ components/Form.vue
@@ -0,0 +1,32 @@
+<template>
+	<form
+		action="#"
+		class="flex flex-col gap-4"
+	>
+		<component v-for="field in fields"
+			:is="formComponents[field.component]"
+			v-bind="field"
+		/>
+	</form>
+</template>
+
+<script setup>
+	// v-model="form[field.name]"
+	// :error="errors[field.name]"
+
+	var {fields} = defineProps({
+		fields: Array
+	});
+
+	var formComponents = {
+		Input: resolveComponent('FormInput'),
+		Image: resolveComponent('FormImage'),
+		Checkbox: resolveComponent('FormCheckbox'),
+		Radio: resolveComponent('FormRadio'),
+		Select: resolveComponent('FormSelect'),
+		ArrList: resolveComponent('FormArrList'),
+		ObjList: resolveComponent('FormObjList'),
+		Textarea: resolveComponent('FormTextarea'),
+	};
+
+</script>
--- pages/patients/[patient_id]/receptions/create.vue
+++ pages/patients/[patient_id]/receptions/create.vue
@@ -9,27 +9,12 @@
 		<textarea name="description" v-model="description"></textarea>
 	</label>
 	<DpvEditor v-bind="{patient}"/> -->
-	<component v-for="field in fields"
-		:is="formComponents[field.component]"
-		v-bind="field"
-	/>
+	<Form :fields="fields"/>
 
 </NuxtLayout>
 </template>
 
 <script setup>
-	// v-model="form[field.name]"
-	// :error="errors[field.name]"
-	var formComponents = {
-		Input: resolveComponent('FormInput'),
-		Image: resolveComponent('FormImage'),
-		Checkbox: resolveComponent('FormCheckbox'),
-		Radio: resolveComponent('FormRadio'),
-		Select: resolveComponent('FormSelect'),
-		ArrList: resolveComponent('FormArrList'),
-		ObjList: resolveComponent('FormObjList'),
-		Textarea: resolveComponent('FormTextarea'),
-	};
 	
 
 	const id = useRoute().params.patient_id;
@@ -38,8 +23,6 @@
 
 	// const patient = await useFetch(`/api/patients/${id}`);
 	var {data: fields} = await useFetch(`/api/fields`);
-	console.log(fields.value)
-
 
 </script>
 
