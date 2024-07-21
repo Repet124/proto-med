cc51f1a3995e77349e86abd21d5f0352c03e0534
Oleg Bondarenko <hlop4ik124@gmail.com>
2024-07-21 22:57:47 +0300

    disabled functional

--- components/Btn.vue
+++ components/Btn.vue
@@ -3,7 +3,12 @@
 	<component
 		:is="type === 'link' ? NuxtLink : 'button'"
 		:to="to"
-		:class="`w-fit text-white bg-slate-800 ${sizes[size]} px-4 py-2 rounded-md hover:bg-slate-700 transition`"
+		class="w-fit text-white  px-4 py-2 rounded-md transition"
+		:class="[
+			sizes[size],
+			disabled ? 'bg-gray-500' : 'bg-slate-800 hover:bg-slate-700'
+		]"
+		:disabled="disabled"
 		@click="type === 'back' ? useRouter().back() : $emit('click')"
 	>
 		<slot>{{ type === 'back' ? 'Назад' : 'Кнопка' }}</slot>
@@ -24,7 +29,7 @@
 <script setup>
 	var NuxtLink = resolveComponent('NuxtLink');
 	
-	var { type, to, size } = defineProps({
+	defineProps({
 		type: {
 			type: String,
 			validator: (val) => ['btn', 'link', 'back'].includes(val),
@@ -35,6 +40,10 @@
 			type: String,
 			validator: (val) => Object.keys(sizes).includes(val),
 			default: 'xl'
+		},
+		disabled: {
+			type: Boolean,
+			default: false
 		}
 	});
 </script>
