b75cfa9f7350818133d7dfae7a464e8d588b1a4b
Oleg Bondarenko <hlop4ik124@gmail.com>
2024-07-22 22:07:49 +0300

    first step of update to vue 3

--- components/form/ImagePreview.vue
+++ components/form/ImagePreview.vue
@@ -1,14 +1,14 @@
 <template>
 
-	<div class="form-imagePreview">
+	<div class="relative flex items-center w-48 h-40 ">
 		<button
 			type="button"
-			class="btn btn-danger material-symbols-outlined"
+			class="absolute -top-4 -right-4 bg-red-600"
 			@click="$emit('delete')"
 		>
 			Delete
 		</button>
-		<img :src="src">
+		<img class="max-w-full max-h-full" :src="src">
 	</div>
 
 </template>
