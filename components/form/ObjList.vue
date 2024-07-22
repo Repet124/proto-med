5528a425a882e6aec085bb11696e08bcf9191abf
Oleg Bondarenko <hlop4ik124@gmail.com>
2024-07-22 14:45:50 +0300

    ObjList component was updated

--- components/form/ObjList.vue
+++ components/form/ObjList.vue
@@ -2,110 +2,52 @@
 <FormLabel :title="label || name.charAt(0).toUpperCase() + name.slice(1)">
 
 	<FormObjListItem
-		v-for="(item, index) in list"
-		:name="item[0]"
-		:value="item[1]"
-		:disabledDelete="list.length === min"
-		@ch="changeItem(index, $event)"
-		@delete="delItem(index)"
+		v-for="(item, index) in model"
+		:name="item.name"
+		:value="item.val"
+		:disabledDelete="model.length === minCount"
+		@input="change(model => model[index] = $event)"
+		@delete="change(model => model.splice(index, 1))"
 	/>
 
 	<Btn
 		type="btn"
 		size="sm"
-		:disabled="list.length === max"
-		@click="addItem"
+		:disabled="model.length === maxCount"
+		@click="change(model => model.push({name:'',val:''}))"
 	>
 		Add Item
 	</Btn>
 
-	<span v-if="!!error" class="form-err">{{error}}</span>
+	<span v-if="!!error" class="text-red-600">{{error}}</span>
 
 </FormLabel>
 </template>
 
-<script>
+<script setup lang="ts">
 
-import ObjListItem from './ObjListItem.vue';
+	type Props = {
+		name: string;
+		label?: string;
+		error?: string;
+		minCount?: number;
+		maxCount?: number;
+		initCount?: number;
+	};
 
+	var { initCount } = withDefaults(defineProps<Props>(), {
+		minCount: 1,
+		maxCount: Infinity,
+		initCount: 3,
+	});
 
-export default {
+	var model = defineModel();
+	model.value = Array(initCount).fill().map(() => {name:'', val:''});
 
-	data() {
-		return {
-			list: this.generate(this.modelValue ? this.modelValue : null)
-		}
-	},
-
-	props: {
-		modelValue: Array,
-		error: String,
-
-		// fields from backend
-		name: String,
-		label: String,
-		count: Object
-	},
-
-	watch: {
-		list(newValue) {
-			this.$emit('update:modelValue', newValue.map(item => ({name: item[0], value: item[1]})));
-			this.$forceUpdate();
-		}
-	},
-
-	// beforeUpdate() {
-	// 	console.log(this.list)
-	// },
-
-	methods: {
-		generate(val) {
-
-			let list = [];
-
-			if (val) {
-				list = val.map(item => [item.name, item.value]);
-			}
-
-			let defaultCount = this.count && this.count.default ? this.count.default : 3;
-
-			for (let i = 0; i < (defaultCount - list.length); i++) {
-				list.push(['','']);
-			}
-			return list;
-		},
-		changeItem(index, value) {
-			let newList = Array.from(this.list);
-			newList[index] = value;
-			this.list = newList;
-		},
-		delItem(index) {
-			let newList = Array.from(this.list);
-			newList.splice(index, 1);
-			this.list = newList
-		},
-		addItem() {
-			let newList = Array.from(this.list);
-			newList.push(['','']);
-			this.list = newList;
-		}
-	},
-
-	computed: {
-		title() {
-			return this.label ? this.label : this.name.charAt(0).toUpperCase() + this.name.slice(1);
-		},
-		max() {
-			return this.count && this.count.max ? this.count.max : Infinity;
-		},
-		min() {
-			return this.count && this.count.min ? this.count.min : 1;
-		}
-	},
-
-	components: {
-		ObjListItem
+	function change(fnModelChange) {
+		var newModel = Array.from(model.value);
+		fnModelChange(newModel);
+		model.value = newModel;
 	}
-}
 
 </script>
